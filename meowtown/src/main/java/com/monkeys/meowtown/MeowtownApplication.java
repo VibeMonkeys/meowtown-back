b37dfbde253bb54483deeef61edcfdc9d7897d1d
kimkyunghun3 <kimkyunghun3@naver.com>
2025-08-14 00:18:08 +0900

    ✨ 초기 설정

---
+++ meowtown/src/main/java/com/monkeys/meowtown/MeowtownApplication.java
@@ -0,0 +1,13 @@
+package com.monkeys.meowtown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowtownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowtownApplication.class, args);
+	}
+
+}
